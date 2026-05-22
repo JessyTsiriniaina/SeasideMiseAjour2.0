0ea0606e9d13c9a890c82c6233d7731cfcf55c6d
Jordio-Dio <razafimahatratradiolin@gmail.com>
2026-05-22 22:40:16 +0300

    Itialisation backend Spring Boot

---
+++ backend/src/main/java/com/seaside/seaside_api/SeasideApiApplication.java
@@ -0,0 +1,13 @@
+package com.seaside.seaside_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeasideApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeasideApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/seaside/seaside_api/SeasideApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seaside.seaside_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeasideApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
